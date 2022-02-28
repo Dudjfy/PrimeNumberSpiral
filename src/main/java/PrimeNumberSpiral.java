bd2ea721e702963f496830a447485183e074ebb3
Dmitry Chirin <chirin.dima@gmail.com>
2022-02-28 20:58:52 +0100

    removed commented code

--- src/main/java/PrimeNumberSpiral.java
+++ src/main/java/PrimeNumberSpiral.java
@@ -11,36 +11,19 @@ public class PrimeNumberSpiral {
 
         int x = Math.floorDiv(w, 2);
         int y = Math.floorDiv(h, 2);
-//        System.out.println(x + " " + y);
 
         String[][] board = new String[h][w];
-//        for (int row = 0; row < board.length; row++) {
-//            for (int col = 0; col < board[row].length; col++) {
-//                board[row][col] = "";
-//            }
-//        }
-
 
         int range = w * h;
         int startAt = 2;
         for (int i = startAt; num < range; i++) {
-//            System.out.println("i: " + i + ", dir:" + direction % 360 + ", cos:" + (int) Math.cos(Math.toRadians(direction)) + ", sin:" + (int) Math.sin(Math.toRadians(direction)) +  " - ");
             for (int j = 0; j < Math.floorDiv(i, 2); j++) {
-//              System.out.println(num + " - X" + x + " Y" + y);
-//              System.out.println("i: " + i + ", dir:" + direction % 360
-//                        + ", cos:" + (int) Math.cos(Math.toRadians(direction))
-//                        + ", sin:" + (int) Math.sin(Math.toRadians(direction))
-//                        + ", X" + x
-//                        + " Y" + y
-//                        + " - ");
-
                 board[y][x] = Integer.toString(num);
                 x += (int) Math.cos(Math.toRadians(direction));
                 y += (int) Math.sin(Math.toRadians(direction));
                 num++;
             }
             direction -= 90;
-//            System.out.println();
         }
 
         printBoard(board);
